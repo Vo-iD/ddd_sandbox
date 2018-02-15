cb8726b0ef9fbf1207db974f6733447a3339f90b
Bohdan Simianyk <Bohdan_Simianyk@epam.com>
2018-02-15 15:54:47 +0200

    Added nunit tests.

---
+++ DDD.Sandbox/DDD.NunitTests/FirstTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace DDD.NunitTests
+{
+    [TestFixture]
+    public class FirstTests
+    {
+        [Test]
+        public void SuccessTest()
+        {
+
+        }
+    }
+}
